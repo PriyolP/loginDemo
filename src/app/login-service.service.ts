32ac5dceed59d580853371751408ea763b67995e
preyool <preyool92@gmail.com>
2020-04-02 16:20:08 +0530

    sm

--- src/app/login-service.service.ts
+++ src/app/login-service.service.ts
@@ -9,10 +9,13 @@ export class LoginServiceService {
   ];
   public imgLink = '../assets/download.png';
   public productList = [
-      { id: 0, img: this.imgLink, companyName: 'Nokia', image: 'linkPic-Sorce', quality: 1, amount: 10, quantity: 0 },
-      { id: 1, img: this.imgLink, companyName: 'Nokia', image: 'linkPic-Sorce', quality: 2, amount: 10, quantity: 0 },
-      { id: 2, img: this.imgLink, companyName: 'Nokia', image: 'linkPic-Sorce', quality: 5, amount: 10, quantity: 0 }
-    ];
+    { id: 0, img: this.imgLink, companyName: 'Tata', image: 'linkPic-Sorce', quality: 1, amount: 10, quantity: 0 },
+    { id: 1, img: this.imgLink, companyName: 'Swift', image: 'linkPic-Sorce', quality: 2, amount: 10, quantity: 0 },
+    { id: 2, img: this.imgLink, companyName: 'Indica', image: 'linkPic-Sorce', quality: 5, amount: 10, quantity: 0 },
+    { id: 3, img: this.imgLink, companyName: 'Verna', image: 'linkPic-Sorce', quality: 1, amount: 10, quantity: 0 },
+    { id: 4, img: this.imgLink, companyName: 'Samsung', image: 'linkPic-Sorce', quality: 2, amount: 10, quantity: 0 },
+    { id: 5, img: this.imgLink, companyName: 'Nokia', image: 'linkPic-Sorce', quality: 5, amount: 10, quantity: 0 }
+  ];
 
   constructor() { }
 
